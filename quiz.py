01b8f18d142d1744a71d94cce3388b10db1196a9
kevinmarindev <kevin.marin7@hotmail.com>
2025-08-21 21:16:58 -0500

    scripting with python

--- quiz.py
+++ quiz.py
@@ -1,16 +1,43 @@
 import pathlib
+import random
 from quizData import capitals
-## loop 35 times to create 35 quizes
-# for i in range(5):
-#     print(i)
+print("starting program")
+print("=======================")
+print(capitals)
 
-print(pathlib.Path().cwd())
-p = pathlib.Path("files/file_one.txt")
-print(p.suffix)
+# print(pathlib.Path().cwd())
+p = pathlib.Path("files/")
+print("my path", pathlib.Path().cwd() / p)
 if p.exists():
     print('yes valid')
 
 for i in range(1):
-    file = open(p, "a", encoding="utf-8")
-    file.write("\n new line")
-    file.close()
+    quiz = open(f"files/quiz_{i}", "w", encoding="utf-8")
+    answer_sheet = open(f"files/answer_{i}", "w", encoding="utf-8")
+    ##header for quiz file
+    quiz.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quiz.write((' ' * 20) + f'State Capitals Quiz {i}')
+    quiz.write('\n\n')
+
+    ##capitals
+    states = list(capitals.keys())
+    print("random:", states)
+    random.shuffle(states)
+
+    ##get correct answer and 3 wrong answers for the quiz
+    for i in range(5):
+        correct_answer = capitals[states[i]]
+        wrong_answers = list(capitals.values())
+        del wrong_answers[wrong_answers.index(correct_answer)]
+        sample = random.sample(wrong_answers, 3)
+        all_options = sample + [correct_answer]
+        print(all_options)
+
+        ## write to file
+        quiz.write(f"\n{i + 1}. Capital of {states[i]}? \n")
+        options = ["a","b","c", "d"]
+        for j in range(4):
+            quiz.write(f"{options[j]}. {all_options[j]} \n")
+        # quiz.close()
+        ##answer key
+        answer_sheet.write(f"{i + 1}. {correct_answer} \n")
