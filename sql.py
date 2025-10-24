0f990efecdab0f7f2a2c90be6f76e6e89a82b610
kevinmarindev <kevin.marin7@hotmail.com>
2025-10-24 00:34:48 -0500

    minor change

--- sql.py
+++ sql.py
@@ -1,8 +1,14 @@
 import sqlite3
 from pathlib import Path
 
-the_path = Path("cats")
-home_path = Path.home()
-print(the_path)
-print(Path.is_absolute(the_path))
-print(Path.is_absolute(home_path))
+# the_path = Path("cats")
+# home_path = Path.home()
+# print(the_path)
+# print(Path.is_absolute(the_path))
+# print(Path.is_absolute(home_path))
+
+conn = sqlite3.connect('./files/sample.db', isolation_level="None")
+#The connection stays open as long as the conn object exists and the script is running. When the script finishes, Python will close the connection automatically.
+
+
+conn.execute("CREATE TABLE IF NOT EXISTS dogs(name TEXT, age INTEGER);")
